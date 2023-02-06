34949537f04672a858bbe3b807ff79f8912c0521
Alix Peigue <irobot.moi@gmail.com>
2023-02-06 20:06:29 +0100

    js (not working)

---
+++ ai/test-js.js
@@ -0,0 +1,14 @@
+//const readline = require("readline");
+
+const line = readLine();
+
+const stringArray = str.split(/(\s+)/);
+
+if(stringArray[2]=="0"){
+  readline();
+}
+
+while(1){
+  console.log(1)
+  readline();
+}
