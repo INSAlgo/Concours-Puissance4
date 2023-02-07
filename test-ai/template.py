716ba6e4ec33d873d0d969507f631a45d31c5ecd
Lysquid <romainbenoit21560@gmail.com>
2023-02-07 16:46:09 +0100

    better template

--- test-ai/template.py
+++ test-ai/template.py
@@ -1,8 +1,6 @@
-import random
-
 W, H, N, S = map(int, input().split())
 
-# Fonction pour savoir a quelle hauteur va tomber pion
+# Fonction pour savoir à quelle hauteur va tomber pion
 def fallHeight(board, x):
     y = len(board[x])
     while board[x][y - 1] == 0 and y > 0:
@@ -11,27 +9,24 @@ def fallHeight(board, x):
 
 # Calcul du meilleur coup à jouer étant donné l'état de la grille
 def strategy(board):
-    # Liste des colonnes, dans un ordre aléatoire
-    columns = list(range(0, W))
-    random.shuffle(columns)
-    for x in columns:
-        # On cherche une colonne non pleine
-        if board[x][H - 1] == 0:
-            return x
-    return -1
+    # Recherche de la première colonne non pleine
+    x = 0
+    while board[x][H - 1] != 0:
+        x += 1
+    return x
 
 def main():
-    # Initialisation
-    board = [[0 for _ in range(H)] for _ in range(W)]
 
+    board = [[0 for _ in range(H)] for _ in range(W)]
     player = 0
+
     while True:
         player = player % N + 1
 
         # Tour de notre IA
         if player == S:
             x = strategy(board)
-            print(f"> {x} {fallHeight(board, x)}")
+            print(f"> {x} {fallHeight(board, x)}")  # Debug
             print(x)
         # Tour adversaire
         else:
