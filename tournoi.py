12ff01414247cb2edbef577feaed3c5ada502908
Lysquid <romainbenoit21560@gmail.com>
2023-02-24 07:23:28 +0100

    semaphore

--- tournoi.py
+++ tournoi.py
@@ -12,15 +12,6 @@ SRCDIR = "ai"
 MAX_PARALLEL_PROCESSES = 200
 ALLOWED_EXTENSIONS = ['.py', '.js', '', '.out', '.class']
 
-def split(arr, size = MAX_PARALLEL_PROCESSES):
-    arrs = []
-    while len(arr) > size:
-        pice = arr[:size]
-        arrs.append(pice)
-        arr = arr[size:]
-    arrs.append(arr)
-    return arrs
-
 def explore(dirname: str) -> list[dict[str, str]]:
     path_to_files = list()
     for root, _, files in os.walk(dirname):
@@ -48,6 +39,57 @@ def print_scores(scoreboard, nbGames) -> None:
     for i, (name, score) in enumerate(scoreboard):
         print(f"{i+1}. {name} ({score})")
 
+async def safe_game(semaphore, players, args):
+    async with semaphore:
+        return await puissance4.main(list(players) + args)
+
+async def tournament(rematches, nb_players, src_dir, args):
+    print(f"Tournament for {SRCDIR} folder")
+
+    # Compile programs
+    subprocess.run(('make', f"SRCDIR={src_dir}"), capture_output=True)
+
+    # Get all programs
+    files = explore("out")
+    paths = [file["path"] for file in files if not file["path"].startswith(".")]
+
+    # Initialize score
+    scores = {file["filename"] : 0 for file in files}
+    game_nb = 0
+
+    # Create list of coroutines to run
+    games = list()
+    semaphore = asyncio.Semaphore(MAX_PARALLEL_PROCESSES)
+
+    for combinations in itertools.combinations(paths, nb_players):
+        for players in itertools.permutations(combinations):
+            for _ in range(rematches):
+                games.append(safe_game(semaphore, players, args))
+
+    nb_games = len(games)
+
+    # Awaiting and printing results
+    origin_stdout = sys.stdout
+    devnull = open(os.devnull, "w")
+
+    # for subGames in games :
+
+    sys.stdout = devnull
+    results = await asyncio.gather(*games)
+    sys.stdout = origin_stdout
+
+    for players, winner, errors in results:
+        game_nb += 1
+        if winner:
+            scores[str(winner)] += 1
+        print_game_results(game_nb, nb_games, players, winner, errors)
+
+    scoreboard = sorted(scores.items(), key=lambda score: score[1], reverse=True)
+    print_scores(scoreboard, nb_games)
+    subprocess.run(('make', 'clean'), capture_output=True)
+
+    return scoreboard
+
 def main():
 
     parser = argparse.ArgumentParser()
@@ -66,55 +108,6 @@ def main():
         sys.stderr = log_file
 
     asyncio.run(tournament(rematches, nb_players, src_dir, remaining_args))
-
-async def tournament(rematches, nb_players, src_dir, args):
-    print(f"Tournament for {SRCDIR} folder")
-
-    # Compile programs
-    subprocess.run(('make', f"SRCDIR={src_dir}"), capture_output=True)
-
-    # Get all programs
-    files = explore("out")
-    paths = [file["path"] for file in files if not file["path"].startswith(".")]
-
-    # Initialize score
-    scores = {file["filename"] : 0 for file in files}
-    game_nb = 0
-
-    # Create list of coroutines to run
-    games = list()
-
-    for combinations in itertools.combinations(paths, nb_players):
-        for players in itertools.permutations(combinations):
-            for _ in range(rematches):
-                games.append(puissance4.main(list(players) + args))
-
-    nb_games = len(games)
-
-    # Make sublists of size MAX_PARALLEL_PROCESSES
-    games = split(games)
-
-    # Awaiting and printing results
-    origin_stdout = sys.stdout
-    devnull = open(os.devnull, "w")
-
-    for subGames in games :
-
-        sys.stdout = devnull
-        results = await asyncio.gather(*subGames)
-        sys.stdout = origin_stdout
-
-        for players, winner, errors in results:
-            game_nb += 1
-            if winner:
-                scores[str(winner)] += 1
-            print_game_results(game_nb, nb_games, players, winner, errors)
-
-    scoreboard = sorted(scores.items(), key=lambda score: score[1], reverse=True)
-    print_scores(scoreboard, nb_games)
-    subprocess.run(('make', 'clean'), capture_output=True)
-
-    return scoreboard
 
 if __name__ == '__main__':
     main()
